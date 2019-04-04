13d5176c6d5002fb98d2dcc83664dc5f9b7224a3
Admin <5828537@gmail.com>
2019-04-04 15:29:20 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Car } from './car';
+import {CarService} from './app/car.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cars';
+export class AppComponent implements OnInit{
+  cars: Car[];
+  error = '';
+  success = '';
+
+  constructor(private carService: CarService) {
+
+  }
+
+  ngOnInit() {
+    this.getCars();
+  }
+  getCars(): void {
+    this.carService
+  }
+
+
 }
